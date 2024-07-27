4e5c76077484cdef3e040e74cd58a7298b6512d8
Vishnu P K <vishnuvpk05@gmail.com>
2024-07-28 01:34:42 +0530

    feat(api): leader routes init

--- server/routes/volunteer.routes.js
+++ server/routes/volunteer.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsers } from "../controllers/users.controller.js";
+import { updateProfile } from "../controllers/profile.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsers);
-
+router.post("/updateProfile", protectRoute, updateProfile);
 export default router;
