5280f564193522cdf4367203e6d0a71c5f92176a
Vishnu P K <vishnuvpk05@gmail.com>
2024-07-28 07:34:19 +0530

    feat(api): create previous class briefs

---
+++ server/controllers/briefs.controller.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js";
+import Student from "../models/student.model.js";
+
+export const createBrief = async (req, res) => {
+  try {
+    const volunteer = User.findById(req.user);
+
+    if (!volunteer || !volunteer.assignedStudent) {
+      return res.status(400).json({ message: "No assigned student found" });
+    }
+
+    const { content } = req.body;
+    if (!content) {
+      return res.status(400).json({ message: "Brief content is required" });
+    }
+
+    const student = await Student.findById(volunteer.assignedStudent);
+
+    if (!student) {
+      return res.status(400).json({ message: "Assigned student not found" });
+    }
+
+    const newBrief = {
+      date: new Date(),
+      content,
+    };
+
+    student.lastBrief.push(newBrief);
+    student.lastVolunteer = volunteer._id;
+
+    await student.save();
+
+    res
+      .status(200)
+      .json({ message: "Brief created successfully", brief: newBrief });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
--- server/routes/volunteer.routes.js
+++ server/routes/volunteer.routes.js
@@ -2,7 +2,10 @@ import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import { updateProfile } from "../controllers/profile.controller.js";
 
+import { createBrief } from "../controllers/briefs.controller.js";
+
 const router = express.Router();
 
 router.post("/updateProfile", protectRoute, updateProfile);
+router.post("/createBrief", protectRoute, createBrief);
 export default router;
